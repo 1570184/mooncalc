f4988faae637e5c3f41d873cb038558523d68f25
1570184 <lebubuslubus@gmail.com>
2025-01-24 13:36:47 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -14,45 +14,101 @@ levelBg[3] = [ 'url("https://i.ibb.co/r6cFKDz/image.png")' ]
 levelBg[4] = [ 'url("https://i.ibb.co/Q9CVRq3/image.png")' ]
 levelBg[5] = [ 'url("https://i.ibb.co/Z8WzxQ6/image.png")' ]
 
+if (localStorage.getItem("storedAge")) { $("#age").val(localStorage.getItem("storedAge")); }
+if (localStorage.getItem("storedSkill")) { $("#skill").val(localStorage.getItem("storedSkill")); }
+if (localStorage.getItem("storedLevel")) { $("#level").val(localStorage.getItem("storedLevel"));}
+if (localStorage.getItem("storedFeather")) { $("#feather").val(localStorage.getItem("storedFeather"));}
+
+var currHp;
+var currIter = 1;
+$("#warning").hide();
+
+anyChange();
+
 function anyChange() {
   
-  console.log("anyChange");
-  
-  var age = $( "#age option:selected" ).val();
-  var skill = $( "#skill" ).val();
-  var level = $( "#level" ).val();
+  age = $( "#age option:selected" ).val();
+  skill = $( "#skill" ).val();
+  level = $( "#level" ).val();
   var hp = $( "#hp" ).val();
   var feather = $( "#feather" ).val();
   
-  var getCell = damage [level][skill];
-  var result = hp-Math.round(getCell*age*(100*(1-feather*0.02)));
+  localStorage.setItem("storedAge", age); console.log (localStorage.getItem("storedAge"));
+  localStorage.setItem("storedSkill", skill); console.log (localStorage.getItem("storedSkill"));
+  localStorage.setItem("storedLevel", level); console.log (localStorage.getItem("storedLevel"));
+  localStorage.setItem("storedFeather", feather); console.log (localStorage.getItem("storedFeather"));
   
-  console.log(result);
+  if (age > 1) {
+    $("#feather").attr("max", 2);
+      if (feather > 2) {$("#feather").val(2);}
+  } else {
+    $("#feather").attr("max", 10);
+  }
+  console.log($("#feather").attr("max"));
+  
+  if (feather > 0) {
+    $("#feather").css("border", "solid, green")
+  } else {
+    $("#feather").css("border", "inherit")
+         }
+
+  var getCell = damage [level][skill];
+  var result = hp-(Math.round(currIter*getCell*age*(100*(1-feather*0.02))));
+
   
   if ( result<1 ) {
     output = "&#x1F480;";
     $("#strip2").css("width", "0%")
+    currHp = 0;
   } else {
     output = result + "%";
     $("#strip2").css("width", output)
+    currHp = result;
   }
   
   $("#result").html (output);
   $("#strip2").css("width", output)
+  captionInfo(level, skill, age, feather, result)
   
-  if (age > 1) {
+}  // общая для всех изменений функция
+
+function captionInfo (height, skl, ofage, buff, spare) {
+    if (ofage > 1) {
     var ageText = "котёнка";
   } else {
     var ageText = "взрослого кота";
   }
   
-  $("#caption").text("...останется после падения с " + level + " высоты у " + ageText + " с " + skill + " ЛУ");
-} // общая для всех изменений функция
+  if (buff == 0) {
+    var buffText = "."
+  } else {
+    var buffText = " и " + buff + " перьями"
+  }
+  
+  $("#caption").text("...останется после " + currIter + "-го падения с " + height + " высоты у " + ageText + " с " + skl + " ЛУ" + buffText);
+  
+  if (spare < 10 && spare > 0 && height > 2 ) {
+    $("#warning").show()
+  } else { $("#warning").hide() }
+  
+}
 
-$( ".param" ).change(function(){ anyChange() });
+$( ".param" ).change(function(){ 
+  currIter=1;
+  anyChange() 
+});
+
 $("#reset").click(function(){ 
-    $("#strip2").css("width", "100%")
-    $("#result").text("100%");
+    currIter=0;
     $("#hp").val(100);
-    $("#caption").text("Снова здоровы! Идём падать?");
+    currHp = 100;
+    $("#result").text("100%");
+    $("#strip2").css("width", "100%")
+    $("#warning").hide()
+    $("#caption").text("Снова здоровы! Откуда идём падать?");
+}) // Сброс хп до 100%
+
+$("#iteration").click(function(){ 
+    currIter++;
+    anyChange();
 })
